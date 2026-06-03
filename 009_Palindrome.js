c6ee7246932b95fe3587228d7b69e44d278f3b72
Avi <a.avneeshkumar@gmail.com>
2026-06-03 13:31:53 +0530

    Palindrome

---
+++ 009_Palindrome.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+
+    if(x<0) return false;
+
+    let original = x;
+    let reversedNum = 0;
+
+    while(original>0){
+        const lastDigit = original % 10;
+        reversedNum = (reversedNum * 10) + lastDigit;
+        original = Math.floor(original/10);
+    }
+    return x === reversedNum;
+}
+
+console.log(isPalindrome(121));
